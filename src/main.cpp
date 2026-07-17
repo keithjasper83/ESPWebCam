5fb8fec90a2c0c13b3a22a4ea9d2b24c763acc68
Keith Jasper <keith_83@icloud.com>
2026-07-17 15:47:33 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,126 @@
+/**********************************************************************
+  Filename    : Video Web Server
+  Description : The camera images captured by the ESP32S3 are displayed on the web page.
+  Auther      : www.freenove.com
+  Modification: 2026/05/16
+**********************************************************************/
+#include "esp_camera.h"
+#include <WiFi.h>
+#include "sd_read_write.h"
+
+// Select camera model
+#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
+
+#include "camera_pins.h"
+
+const char* ssid     = "KJ iPhone";   //input your wifi name
+const char* password = "hello123";   //input your wifi passwords
+camera_config_t config;
+
+void cameraInit(void);
+void startCameraServer();
+
+void setup() {
+
+  Serial.begin(115200);
+  delay(500);  // Wait for serial monitor to connect
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  cameraInit();
+  sdmmcInit();
+  removeDir(SD_MMC, "/video");
+  createDir(SD_MMC, "/video");
+  
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(10000);
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");}
+
+void cameraInit(void){
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;  // Increase to 20 MHz for better frame capture
+  config.frame_size = FRAMESIZE_VGA;  // 640x480 for better quality
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;  // Increase quality (1-63, higher=better but larger)
+  config.fb_count = 1;
+  
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    if(err==ESP_ERR_NOT_SUPPORTED){
+      config.pixel_format = PIXFORMAT_RGB565;
+      esp_err_t err = esp_camera_init(&config);
+      if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        return;
+      }
+    }
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  // drop down frame size for higher initial frame rate
+  uint16_t pid = s->id.PID;
+  if(pid == OV2640_PID){
+    s->set_hmirror(s, 1);
+    s->set_vflip(s, 1);     
+  }
+  else if(pid == OV3660_PID){
+    s->set_hmirror(s, 1);
+    s->set_vflip(s, 0);     
+  }
+  else if(pid == GC2145_PID){
+    s->set_hmirror(s, 0);
+    delay(500);
+    s->set_vflip(s, 0);      
+  }
+  else if(pid == GC0308_PID){
+    s->set_hmirror(s, 0);
+    delay(500);
+    s->set_vflip(s, 0);     
+  }
+  else{
+    s->set_hmirror(s, 1);
+    s->set_vflip(s, 0);       
+  }
+  s->set_brightness(s, 1);  // Slightly increase brightness
+  s->set_saturation(s, 0);  // Reduce saturation
+  s->set_ae_level(s, -3);   // Set exposure compensation level
+}
